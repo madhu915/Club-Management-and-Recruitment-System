c040424cc34ab42acb1f3f7f173effe248ac3304
M Madhumithaa <mmadhumithaa09@gmail.com>
2022-01-29 11:33:22 +0530

    updated login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,40 @@
 import React from 'react';
 import logo from '../assets/logo.png'
 import user from '../assets/user.png'
+import { useState } from "react";
 
 function Login() {
+
+    const [username, setusername] = useState("");
+  const [password, setpassword] = useState("");
+  const[usertype,setusertype]=useState("President");
+  const onChangeHandler = (fieldName, value)=>{
+    if(fieldName === "username"){
+      setusername(value);
+    }
+    else if(fieldName==="password"){
+      setpassword(value);
+    }
+    else if(fieldName==="usertype"){
+      setusertype(value);
+    }
+  }
+  const onSubmitHandler = (e)=>{
+    e.preventDefault();
+    if(username.trim() === "" || password.trim() ===""){
+      alert("required both field");
+    }
+    else{
+        if(username==="admin@abc.com" && password==="$abc@svce22" && usertype==="President"){
+            alert("Successfully Logged in!!");
+            setusername("");
+            setpassword("");
+        }
+        else{
+            alert("Incorrect Login!!")
+        }
+    }
+  }
   return (
     <div className='black-bubble'>
         
@@ -14,20 +46,20 @@ function Login() {
             <h2 className="bubble-heading"> WELCOME TO ABC CLUB <br/> OF SVCE</h2>
             <p className="bubble-para">Lorem ipsum  urabitur a euismod lorem, nec <br/>laoreet tellus. </p>
         </div>
-        <form action="#">
+        <form action="#" onSubmit={(e)=>{onSubmitHandler(e)}}>
             <div>
                 <h2>LOGIN</h2>
                 <p>Enter your details below</p>
                 
             </div>
             <img src={user} alt="user" className='user-img'></img>
-            <select name="user-type">
+            <select name="user-type" onChange={(e) => onChangeHandler("usertype",e.target.value)}>
                 <option value="Select-User">Select User</option>
                 <option value="President">President</option>
                 <option value="Student">Student</option>
                 </select> <br/>
-            <input type="text" name="user" placeholder="Username"></input> <br/>
-            <input type="text"name="password" placeholder="Password"></input><br/>
+            <input type="text" name="user" placeholder="Username" value={username} onChange={(e)=>{ onChangeHandler("username",e.target.value)}}></input> <br/>
+            <input type="password" name="password" placeholder="Password" value={password} onChange={(e)=>{ onChangeHandler("password",e.target.value)}}></input><br/>
             <button className="submit" type="submit">Login</button>
         </form> 
     </div>
