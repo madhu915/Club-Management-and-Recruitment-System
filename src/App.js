919b36d1b9f2f57b34f90f48ee17f6448bb343d2
Jeevitha10 <65245023+Jeevitha10@users.noreply.github.com>
2022-01-29 08:12:30 +0530

    add student-dashboard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Nav from "./components/nav";
+import Events from "./components/events";
+import Greeting from "./components/greeting";
+import Upcoming from "./components/upcoming";
+import Regevents from "./components/regevents";
+import Footer from "./components/footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<Nav />
+			<Events />
+			<Greeting />
+			<Upcoming />
+			<Regevents />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/greeting.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Greeting() {
+	return (
+		<div>
+			<div class="welcome m-3 mx-5">
+				<h2>
+					<i class="fas fa-user me-3"></i>Welcome back XYZ!
+				</h2>
+			</div>
+			<h4 class="welcome mx-5 my-3 mt-5">Upcoming events</h4>
+		</div>
+	);
+}
+
+export default Greeting;
