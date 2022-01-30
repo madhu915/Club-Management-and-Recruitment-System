22eabd9d80f4c0e774a73f28ff6cf84d230ec51b
M Madhumithaa <mmadhumithaa09@gmail.com>
2022-01-30 16:18:37 +0530

    added events calendar

--- src/components/President.js
+++ src/components/President.js
@@ -5,6 +5,9 @@ import Women from "../assets/images/hack.jpg";
 import Spotlight from "../assets/images/Spotlight.jpg";
 import Welcome from "../assets/images/welcome.jpg";
 import $ from "jquery";
+import { useState } from 'react';
+import Popup from './popup';
+
 $(document).ready(function () {
 	$("#sidebarCollapse").on("click", function () {
 		$("#sidebar").toggleClass("active");
@@ -12,6 +15,11 @@ $(document).ready(function () {
 });
 
 function President() {
+	
+	const [isOpen, setIsOpen] = useState(false);
+	const togglePopup = () => {
+    setIsOpen(!isOpen);
+  }
 	return (
 		<div className="wrapper">
 			<nav id="sidebar">
@@ -70,8 +78,73 @@ function President() {
 
 				<ul className="list-unstyled CTAs">
 					<li>
-						<a href="register" className="download">
+						<a href="#pageSubmenu" className="download" onClick={togglePopup}>
 							Schedules
+							{isOpen && <Popup
+      						content={<>
+					        <b>Calendar Events</b>
+
+							<div class="jzdbox1 jzdbasf jzdcal">
+
+								<div class="jzdcalt">September 2022</div>
+
+									<span>Su</span>
+									<span>Mo</span>
+									<span>Tu</span>
+									<span>We</span>
+									<span>Th</span>
+									<span>Fr</span>
+									<span>Sa</span>
+
+
+									<span class="jzdb"></span>
+									<span class="jzdb"></span>
+									<span class="jzdb"></span>
+									<span class="jzdb"></span>
+									<span class="jzdb"></span>
+									<span class="jzdb"></span>
+									<span>1</span>
+									<span>2</span>
+									<span>3</span>
+									<span>4</span>
+									<span>5</span>
+									<span>6</span>
+									<span>7</span>
+									<span>8</span>
+									<span>9</span>
+									<span class="circle" data-title="Event 1">10</span>
+									<span>11</span>
+									<span>12</span>
+									<span>13</span>
+									<span>14</span>
+									<span>15</span>
+									<span>16</span>
+									<span>17</span>
+									<span>18</span>
+									<span class="circle" data-title="Event 2">19</span>
+									<span>20</span>
+									<span>21</span>
+									<span>22</span>
+									<span>23</span>
+									<span>24</span>
+									<span>25</span>
+									<span>26</span>
+									<span>27</span>
+									<span>28</span>
+									<span>29</span>
+									<span class="circle" data-title="Event 3">30</span>
+									<span class="jzdb"></span>
+									<span class="jzdb"></span>
+									<span class="jzdb"></span>
+									<span class="jzdb"></span>
+									<span class="jzdb"></span>
+									<span class="jzdb"></span>
+								</div>
+							
+					        {/* <button>Close</button> */}
+					      </>}
+      					handleClose={togglePopup}
+					    />}
 						</a>
 					</li>
 				</ul>
